fbd18fa004932c5b15ec9d2031f197e7d5574c5c
Noah Correa <noah.correa28@gmail.com>
2021-08-20 22:41:02 +1000

    Added files for Phase D Motion Tracking

---
+++ controllers/z5206979_MTRN4110_PhaseA/build/release/z5206979_MTRN4110_PhaseA.d
@@ -0,0 +1,6 @@
+build/release/z5206979_MTRN4110_PhaseA.o: z5206979_MTRN4110_PhaseA.cpp \
+ C:\Users\Noah\AppData\Local\Programs\Webots/include/controller/cpp/webots/Robot.hpp \
+ C:\Users\Noah\AppData\Local\Programs\Webots/include/controller/cpp/webots/Motor.hpp \
+ C:\Users\Noah\AppData\Local\Programs\Webots/include/controller/cpp/webots/Device.hpp \
+ C:/Users/Noah/AppData/Local/Programs/Webots/include/controller/c/webots/types.h \
+ C:\Users\Noah\AppData\Local\Programs\Webots/include/controller/cpp/webots/DistanceSensor.hpp
